5372e66e3407ffa7cb4b33615010bc2aa8ecdcbd
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2023-09-23 00:18:27 -0500

    Complete maps

--- main.go
+++ main.go
@@ -3,22 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	// // Option 1
-	// colors := map[string]string {
-	// 	"red": "#ff0000",
-	// 	"green": "#00ff00",
-	// 	"blue": "#0000ff",
-	// }
+	// Option 1
+	colors := map[string]string {
+		"red": "#ff0000",
+		"green": "#00ff00",
+		"blue": "#0000ff",
+	}
 
 	// // Option 2
 	// var colors map[string]string
 
   // // Option 3
-	colors := make(map[string]string)
-	colors["white"] = "#fff"
+	// colors := make(map[string]string)
 
-	// Deleting an item by key
-	delete(colors, "white")
+	// Adding an item to a map
+	// colors["white"] = "#fff"
 
-	fmt.Println(colors)
+	// // Deleting an item by key
+	// delete(colors, "white")
+
+	printMap(colors)
 }
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
+}
+
+// Maps are reference types
+// So the reference data is copied, not the map itself.
