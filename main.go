8454a843147345634172a91490200d4e38add7ef
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2023-09-23 00:02:29 -0500

    Deleting an item in a map

--- main.go
+++ main.go
@@ -3,17 +3,22 @@ package main
 import "fmt"
 
 func main() {
+	// // Option 1
 	// colors := map[string]string {
 	// 	"red": "#ff0000",
 	// 	"green": "#00ff00",
 	// 	"blue": "#0000ff",
 	// }
 
+	// // Option 2
 	// var colors map[string]string
 
+  // // Option 3
 	colors := make(map[string]string)
-
 	colors["white"] = "#fff"
+
+	// Deleting an item by key
+	delete(colors, "white")
 
 	fmt.Println(colors)
 }
