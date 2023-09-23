8cf1d07466eabb8f9b1f33ece2ac6f28a9fd5fff
Shosuke Noma <47355058+shosukenoma@users.noreply.github.com>
2023-09-23 00:00:48 -0500

    Initializing maps

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// colors := map[string]string {
+	// 	"red": "#ff0000",
+	// 	"green": "#00ff00",
+	// 	"blue": "#0000ff",
+	// }
+
+	// var colors map[string]string
+
+	colors := make(map[string]string)
+
+	colors["white"] = "#fff"
+
+	fmt.Println(colors)
+}
